34d9fae63e499fb4bb7ff6a63bf96eaafbec935d
vladp <vladpolulah30@gmai.com>
2024-11-17 23:26:26 +0200

    Подобавляв класи

--- scr/TeamMember.py
+++ scr/TeamMember.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+# Абстрактний базовий клас TeamMember
 class TeamMember(ABC):
     def __init__(self, name, role):
         self.name = name
@@ -7,4 +8,38 @@ class TeamMember(ABC):
 
     @abstractmethod
     def get_details(self):
+        """Метод для отримання деталей про члена команди"""
         pass
+
+
+# Підклас Player (гравець)
+class Player(TeamMember):
+    def __init__(self, name, skills):
+        super().__init__(name, "Гравець")
+        self.skills = skills
+
+    def get_details(self):
+        """Реалізував мето для отримання деталей про гравця"""
+        return f"{self.role}: {self.name}, Навички: {', '.join(self.skills)}"
+
+
+# Підклас Coach (тренер)
+class Coach(TeamMember):
+    def __init__(self, name, specialization):
+        super().__init__(name, "Тренер")
+        self.specialization = specialization
+
+    def get_details(self):
+        """Метод для отримання деталей про тренера"""
+        return f"{self.role}: {self.name}, Спеціалізація: {self.specialization}"
+
+
+# Підклас Staff (персонал)
+class Staff(TeamMember):
+    def __init__(self, name, position):
+        super().__init__(name, "Персонал")
+        self.position = position
+
+    def get_details(self):
+        """Метод для отримання деталей про персонал"""
+        return f"{self.role}: {self.name}, Посада: {self.position}"
