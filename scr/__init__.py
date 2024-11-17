2f269abfb11bcec5a40306995e859525db627cf7
MUHOTABP <sspoty150@gmail.com>
2024-11-17 23:07:27 +0200

    поправив коменти

--- scr/__init__.py
+++ scr/__init__.py
@@ -1,10 +1,9 @@
-from team_member import Player, Coach, Staff
-from team import Team
-from tournament import Tournament
-from sponsor import Sponsor
-from training_program import TrainingProgram
+# from team_member import Player, Coach, Staff
+# from team import Team
+# from tournament import Tournament
+# from sponsor import Sponsor
+# from training_program import TrainingProgram
 
-# Створення команди
 my_team = Team("Cyber Titans")
 player1 = Player("Alex", ["Sniping", "Strategy"])
 coach = Coach("Chris", "Aggressive Style")
@@ -14,21 +13,21 @@ my_team.recruit_member(player1)
 my_team.recruit_member(coach)
 my_team.recruit_member(analyst)
 
-# Турнір
+# турнір
 tournament = Tournament("World Cup", 100000)
 print(tournament.participate(my_team))
 
-# Тренування
+# тренування
 training = TrainingProgram("Bootcamp", 7)
 print(training.organize_practice(my_team))
 
-# Спонсорство
+# cпонсорство
 sponsor = Sponsor("Tech Corp", ["High performance", "Fanbase"])
 print(sponsor.negotiate(my_team))
 
-# Аналіз
+# аналіз
 print(my_team.analyze_performance())
 
-# Деталі команди
+# деталі команди
 for detail in my_team.get_team_details():
     print(detail)
