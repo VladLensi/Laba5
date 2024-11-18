de7bc8aa143c88780604237d5ecfd05eea18116d
MUHOTABP <sspoty150@gmail.com>
2024-11-18 16:26:27 +0200

    Оновив меню, додав змінні для основних операцій.

--- scr/Sponsor.py
+++ scr/Sponsor.py
@@ -1,7 +1,9 @@
-class Sponsor:
-    def __init__(self, name, requirements):
+class sponsor:
+    def __init__(self, name, requirements, money):
         self.name = name
         self.requirements = requirements
 
     def negotiate(self, team):
         return f"Negotiating sponsorship for team {team.name} with {self.name}."
+    
+
